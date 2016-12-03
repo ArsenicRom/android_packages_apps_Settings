019c2b4625ec699099bb487450f6c601f843355e
ArsenicRom <tumayordeseo5@hotmail.com>
2016-12-03 00:21:00 -0400

    fix fastscroll

--- src/com/android/settings/applications/ManageApplications.java
+++ src/com/android/settings/applications/ManageApplications.java
@@ -305,7 +305,7 @@ public class ManageApplications extends InstrumentedFragment
             lv.setSaveEnabled(true);
             lv.setItemsCanFocus(true);
             lv.setTextFilterEnabled(true);
-            lv.setfastScrollEnabled(true);
+            lv.setFastScrollEnabled(true);
             mListView = lv;
             mApplications = new ApplicationsAdapter(mApplicationsState, this, mFilter);
             if (savedInstanceState != null) {
