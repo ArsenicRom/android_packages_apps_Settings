4ae9475faec86e57bf22fe8babfb367c76485a61
ArsenicRom <tumayordeseo5@hotmail.com>
2016-12-24 22:08:57 -0400

    Re-organizar ajustes statusbar & qs panel

--- src/com/android/settings/arsenic/QsPanel.java
+++ src/com/android/settings/arsenic/QsPanel.java
@@ -13,9 +13,15 @@
 package com.android.settings.arsenic;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.Fragment;
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.support.v7.preference.PreferenceCategory;
@@ -28,24 +34,62 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
+import cyanogenmod.providers.CMSettings;
+
 public class QsPanel extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "QsPanel";
 
-    @Override
-    protected int getMetricsCategory() {
-        return MetricsEvent.APPLICATION;
-    }
+	private static final String PREF_SMART_PULLDOWN = "smart_pulldown";
+
+	private ListPreference mSmartPulldown;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.arsenic_qs_panel);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+	ContentResolver resolver = getActivity().getContentResolver();
+
+        mSmartPulldown = (ListPreference) findPreference(PREF_SMART_PULLDOWN);
+        mSmartPulldown.setOnPreferenceChangeListener(this);
+        int smartPulldown = Settings.System.getInt(resolver,
+                Settings.System.QS_SMART_PULLDOWN, 0);
+        mSmartPulldown.setValue(String.valueOf(smartPulldown));
+        updateSmartPulldownSummary(smartPulldown);
     }
 
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-
-        return true;
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+	ContentResolver resolver = getActivity().getContentResolver();
+	 if (preference == mSmartPulldown) {
+            int smartPulldown = Integer.valueOf((String) newValue);
+            Settings.System.putInt(resolver, Settings.System.QS_SMART_PULLDOWN, smartPulldown);
+            updateSmartPulldownSummary(smartPulldown);
+            return true;
+	}
+        return false;
     }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.APPLICATION;
+    }
+
+     private void updateSmartPulldownSummary(int value) {
+         Resources res = getResources();
+ 
+         if (value == 0) {
+             // Smart pulldown deactivated
+             mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_off));
+         } else if (value == 3) {
+             mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_none_summary));
+         } else {
+             String type = res.getString(value == 1
+                     ? R.string.smart_pulldown_dismissable
+                     : R.string.smart_pulldown_ongoing);
+             mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_summary, type));
+         }
+     }
 }
