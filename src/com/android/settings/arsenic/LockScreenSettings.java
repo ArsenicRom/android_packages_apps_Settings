4e9d5d65140fe015974a3f71ae798778a683d896
ArsenicRom <tumayordeseo5@hotmail.com>
2017-03-10 05:28:41 -0400

    Max Lockscreen Notification count [2/2]

--- src/com/android/settings/arsenic/LockScreenSettings.java
+++ src/com/android/settings/arsenic/LockScreenSettings.java
@@ -11,13 +11,22 @@
 */
 package com.android.settings.arsenic;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceScreen;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import com.android.settings.arsenic.SeekBarPreference;
+import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -26,6 +35,10 @@ import com.android.settings.Utils;
 public class LockScreenSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "MainSettings";
+
+	private static final String LOCKSCREEN_MAX_NOTIF_CONFIG = "lockscreen_max_notif_cofig";
+
+	private SeekBarPreference mMaxKeyguardNotifConfig;
 
     @Override
     protected int getMetricsCategory() {
@@ -37,10 +50,23 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.arsenic_lockscreen);
+
+        mMaxKeyguardNotifConfig = (SeekBarPreference) findPreference(LOCKSCREEN_MAX_NOTIF_CONFIG);
+        int kgconf = Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, 5);
+        mMaxKeyguardNotifConfig.setValue(kgconf);
+        mMaxKeyguardNotifConfig.setOnPreferenceChangeListener(this);
     }
 
+    @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
-        return true;
+	ContentResolver resolver = getActivity().getContentResolver();
+ 	if (preference == mMaxKeyguardNotifConfig) {
+            int kgconf = (Integer) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, kgconf);
+            return true;
+        }
+        return false;
     }
 }
