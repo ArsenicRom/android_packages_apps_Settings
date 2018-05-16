38a7401c6eb5a53e79326a1cd812aa68fd62a2e1
arsenicrom <tumayordeseo5@hotmail.com>
2018-05-16 03:31:56 -0400

    Revert "ColorOM: Support black theme"
    This reverts commit 6cb93f7ae96c200b5230148e08eb502c00a4864d.

--- src/com/android/settings/aoscp/display/ColorManagerFragment.java
+++ src/com/android/settings/aoscp/display/ColorManagerFragment.java
@@ -52,7 +52,6 @@ public class ColorManagerFragment extends DashboardFragment
     private static final String KEY_THEME_AUTO = "theme_auto";
     private static final String KEY_THEME_LIGHT = "theme_light";
     private static final String KEY_THEME_DARK = "theme_dark";
-    private static final String KEY_THEME_BLACK = "theme_black";
 
     List<RadioButtonPreference> mThemes = new ArrayList<>();
 
@@ -110,9 +109,6 @@ public class ColorManagerFragment extends DashboardFragment
             case 2:
                 updateThemeItems(KEY_THEME_DARK);
                 break;
-            case 3:
-                updateThemeItems(KEY_THEME_BLACK);
-                break;
         }
     }
 
@@ -147,10 +143,6 @@ public class ColorManagerFragment extends DashboardFragment
             case KEY_THEME_DARK:
                 Settings.Secure.putInt(getContentResolver(), 
                          Settings.Secure.DEVICE_THEME, 2);
-                break;
-            case KEY_THEME_BLACK:
-                Settings.Secure.putInt(getContentResolver(), 
-                         Settings.Secure.DEVICE_THEME, 3);
                 break;
         }
         updateThemeItems(pref.getKey());
