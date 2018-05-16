b6b48711c7043017b9c513be9883049cf2644f96
arsenicrom <tumayordeseo5@hotmail.com>
2018-05-16 03:33:14 -0400

    Revert "Settings: Introduce Color Manager (ColorOM)"
    This reverts commit df65d1b63506817fdaf9ec067a10fb9f4d88cfc4.

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -21,7 +21,6 @@ import android.provider.SearchIndexableResource;
 
 import com.android.internal.hardware.AmbientDisplayConfiguration;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
-import com.android.settings.aoscp.display.ColorManagerPreferenceController;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.AmbientDisplayPreferenceController;
 import com.android.settings.display.AutoBrightnessPreferenceController;
@@ -108,7 +107,6 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new VrDisplayPreferenceController(context));
         controllers.add(new WallpaperPreferenceController(context));
         //controllers.add(new ThemePreferenceController(context));
-        controllers.add(new ColorManagerPreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
         controllers.add(new ColorModePreferenceController(context));
         return controllers;
