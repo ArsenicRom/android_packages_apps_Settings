74d5aa9ea79813d1d4605297d657f67a646e62f8
arsenicrom <tumayordeseo5@hotmail.com>
2019-02-02 00:29:09 -0400

    cleanup - fix build

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -16,14 +16,12 @@
 
 package com.android.settings.core.gateway;
 
-import com.android.settings.AmbientPlaySettings;
 import com.android.settings.DateTimeSettings;
 import com.android.settings.DeviceAdminSettings;
 import com.android.settings.DisplaySettings;
 import com.android.settings.IccLockSettings;
 import com.android.settings.MasterClear;
 import com.android.settings.PrivacySettings;
-import com.android.settings.preference.DisplayRotation;
 import com.android.settings.Settings;
 import com.android.settings.TestingSettings;
 import com.android.settings.TetherSettings;
@@ -146,12 +144,11 @@ import com.arsenic.settings.fragments.Buttons;
 import com.arsenic.settings.fragments.Display;
 import com.arsenic.settings.fragments.ExpandedDesktopPreferenceFragment;
 import com.arsenic.settings.fragments.Interface;
-import com.arsenic.settings.fragments.Navigation;
+//import com.arsenic.settings.fragments.Navigation;
 import com.arsenic.settings.fragments.ScreenStateToggles;
 import com.arsenic.settings.fragments.SmartPixels;
 import com.arsenic.settings.fragments.Weather;
 import com.arsenic.settings.fragments.ui.DozeFragment;
-import com.arsenic.settings.fragments.PieSettings;
 
 public class SettingsGateway {
 
@@ -283,15 +280,11 @@ public class SettingsGateway {
             RunningServices.class.getName(),
             Interface.class.getName(),
             DozeFragment.class.getName(),
-            AmbientPlaySettings.class.getName(),
-            Navigation.class.getName(),
             SmartPixels.class.getName(),
             Weather.class.getName(),
             Display.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName(),
             ScreenStateToggles.class.getName(),
-            DisplayRotation.class.getName(),
-            PieSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
